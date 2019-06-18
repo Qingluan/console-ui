37ad95ba2ba0d7526e4f63b6ff19f4c8fe30d0db
dark.h <dark.h@some>
2019-06-19 01:45:47 +0800

    ok

--- x_menu_src/text.py
+++ x_menu_src/text.py
@@ -5,10 +5,12 @@ from .log import log
 
 C_R = {'\x1b[%dm'%v: 'COLOR_%s' % k.upper() for k,v in COLORS.items()}
 A_R = {'\x1b[%dm'%v: 'A_%s' % k.upper() for k,v in ATTRIBUTES.items()}
+A_R['\x1b[3m'] = 'A_ITALIC'
 H_R = {'\x1b[%dm'%v: 'COLOR_%s' % k[3:].upper() for k,v in HIGHLIGHTS.items()}
 COLORS_R = {'\x1b[%dm'%v:k for k,v in COLORS.items()} 
 COLOR_SCAN = re.compile(r'(\x1b\[\d+m)')
 delete_sub = re.compile(r'(\x1b\[\d+\w)')
+COLOR_SCAN2 = re.compile(r'(\x1b\[(\d)\;\d+m)')
 
 def ascii2filter(words):
     if isinstance(words, list):
@@ -17,10 +19,50 @@ def ascii2filter(words):
         strings = delete_sub.sub('',words)
     return strings
 
+def fgascii2curses(context,row, col, string, colors=None, now=0,max_width=None):
+    attr = 0 
+    color = 0
+    strings = COLOR_SCAN2.split(string.strip())
+    first = strings.pop(0)
+    if len(first) > 0:
+        if colors.last_use_color:
+            color = colors.last_use_color 
+        if colors.last_use_attr:
+            a = colors.last_use_attr
+            if a in A_R:
+                attr = A_R[a]
+            context.addstr(row, col,first[now:now+max_width], attr | colors.get(color))
+        else:
+            log('Fcol:',col, '\r' in string , 'f len:',len(first), first.encode())
+            log("-----")
+            context.addstr(row, col,first[now:now+max_width -2])
+        col += len(first) % max_width
+    for i in range(len(strings)//3):
+        color,a, msg = strings[i*3:(i+1)*3]
+        msg = delete_sub.sub('',msg)
+        color = re.sub(r'\d\;','',color)
+        a = '\x1b[%sm' % a
+        if a in A_R:
+            attr = getattr(curses, A_R[a])
+        if color in COLORS_R:
+            color = COLORS_R[color]
+        else:
+            color = 0
+        if msg:
+            log('row:',row,'col:',col,'now:',now,'msg len:',len(msg), max_width)
+            if col >= max_width:
+                break
+            context.addstr(row,col,msg[now:now+max_width- col - 1], attr |  colors.get(color))
+            col += len(msg)
+    colors.last_use_color = color
+    colors.last_use_attr = attr
 
 def ascii2curses(context,row,col,string, colors=None,now=0, max_width=None):
     attr = None 
     color = None
+    if COLOR_SCAN2.search(string):
+        fgascii2curses(context,row,col,string, colors=colors,now=now, max_width=max_width)
+        return
     strings = COLOR_SCAN.split(string)
 
     first = strings.pop(0)
